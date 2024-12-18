771fb61a3fcd5faa0b45834ac795b4b4e41a8e4e
yaroslavdrapei <yarikdrapey@gmail.com>
2024-12-18 18:47:26 +0200

    fix: cors error

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import { routes } from './routes';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
 const PORT = 3000;
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use(routes);
