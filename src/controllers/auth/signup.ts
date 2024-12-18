0e9fbaced37238ac40117dc071e3b5a734b68376
yaroslavdrapei <yarikdrapey@gmail.com>
2024-12-18 18:00:53 +0200

    feat: mock signup route

--- src/controllers/auth/signup.ts
+++ src/controllers/auth/signup.ts
@@ -1,7 +1,26 @@
 import { Request, Response } from 'express';
+import { AuthBody } from '../../types/types';
+import { mockUsers } from '../../mock/mockUsers';
+import jwt from 'jsonwebtoken';
 
 export const signupController = (req: Request, res: Response): void => {
-  const { body } = req;
+  const body = req.body as AuthBody;
 
-  res.status(200).json({ message: 'Accepted (signup)', body });
+  const findUser = mockUsers.find((u) => u.username === body.username);
+
+  if (findUser) {
+    res.status(409).json({ message: 'Username already exists' });
+    return;
+  }
+
+  const newUser: AuthBody = {
+    username: body.username,
+    password: body.password
+  };
+
+  mockUsers.push(newUser);
+
+  const token = jwt.sign(newUser, process.env.SIGN_KEY as string);
+
+  res.status(200).json({ token });
 };
