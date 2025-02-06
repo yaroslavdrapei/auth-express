899261b4120eb578acb7da70eb57f0b57465171a
yaroslavdrapei <yarikdrapey@gmail.com>
2025-02-06 21:00:49 +0200

    feat: add static files support

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,16 @@
 import { Router } from 'express';
 import { authRouter } from './auth';
+import path from 'path';
 
 const routes = Router();
+
+routes.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+routes.get('/welcome', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/welcome.html'));
+});
 
 routes.use('/auth', authRouter);
 
